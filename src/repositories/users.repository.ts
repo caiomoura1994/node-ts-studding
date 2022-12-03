9a0139c29d67e7772b7a21858e8e8ff38f839b5d
Caio Moura <caiomoura1994@gmail.com>
2022-12-03 11:14:16 -0300

    :sparkles: Add UserModel in users repository

--- src/repositories/users.repository.ts
+++ src/repositories/users.repository.ts
@@ -1,58 +1,57 @@
 import { hash } from 'bcrypt';
 import { CreateUserDto } from '@dtos/users.dto';
 import { IUser } from '@interfaces/users.interface';
-import { randomInt } from 'crypto';
+import { UserModel } from '@entities/users.entity';
 import { AppError } from '@/exceptions/AppError';
 
 export default class UserRepository {
-    users: IUser[] = []
+    userModel: typeof UserModel
+    constructor() {
+        this.userModel = UserModel;
+    }
     public async userFindAll(): Promise<IUser[]> {
-        return this.users;
+        return await this.userModel.find().exec();
     }
 
     public async userFindById(userId: String): Promise<IUser> {
         if (!userId) throw new AppError(400, "UserId is empty");
 
-        const user = this.users.find((user) => user._id === userId);
-        if (!user) throw new AppError(400, "IUser doesn't exist");
+        const user = await this.userModel.findById(userId);
+        if (!user) throw new AppError(400, "User doesn't exist");
         return user;
     }
 
     public async userCreate(userData: CreateUserDto): Promise<IUser> {
         if (!userData) throw new AppError(400, "userData is empty");
 
-        const findUser = this.users.find((user) => user.email === userData.email);
+        const findUser = await this.userModel.findOne({ email: userData.email })
         if (findUser) throw new AppError(400, `This email ${userData.email} already exists`);
 
         const hashedPassword = await hash(userData.password, 10);
-        const newUser = { ...userData, password: hashedPassword, _id: String(randomInt(4)) }
-        this.users.push(newUser);
-        return newUser;
+        const newUser = { ...userData, password: hashedPassword }
+        const createUserData = await this.userModel.create(newUser)
+        return createUserData;
     }
 
-    public async userUpdate(userId: string, userData: CreateUserDto): Promise<IUser> {
-        if (!userData) throw new AppError(400, "userData is empty");
+    public async userUpdate(userId: String, userData: CreateUserDto): Promise<IUser> {
+        if (!userData || !userId) throw new AppError(400, "userData is empty");
 
         const findUser: IUser = await this.userFindById(userId);
-        if (!findUser) throw new AppError(400, "IUser doesn't exist");
+        if (!findUser) throw new AppError(400, "User doesn't exist");
 
         const hashedPassword = await hash(userData.password, 10);
-        this.users = this.users.filter((user) => {
-            let formattedUser = user
-            if (user._id == userId) formattedUser.password = hashedPassword;
-            return formattedUser
-        })
-
-        const updateUser: IUser = await this.userFindById(userId);
+        const updateUser = await this.userModel.findOneAndUpdate(
+            { _id: userId },
+            { $set: { ...userData, password: hashedPassword } }
+        )
         return updateUser;
     }
 
-    public async userDelete(userId: string): Promise<IUser[]> {
-        if (!userId) throw new AppError(400, "IUser doesn't existId");
+    public async userDelete(userId: string): Promise<void> {
+        if (!userId) throw new AppError(400, "User doesn't existId");
 
         const findUser: IUser = await this.userFindById(userId)
-        if (!findUser) throw new AppError(400, "IUser doesn't exist");
-
-        return this.users.filter((user) => user._id !== userId);
+        if (!findUser) throw new AppError(400, "User doesn't exist");
+        await this.userModel.deleteOne({ _id: userId })
     }
 }
